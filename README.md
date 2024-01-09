dbf74a62215e605c6a826fc73c97c02e42de6722
nurbilu <nurb111@gmail.com>
2024-01-09 19:22:32 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Library project number 1 
+    this is a library project in the process of creation 
+    a full readme would be added when program is finished
+
+### will be finished no more than the 21/01/2024
---
+++ backend/app.py
@@ -0,0 +1,157 @@
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+# app.config['SQLALCHEMY_DATABASE_URI'] ='sqlite:///library.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:MYSQLnur1996##@localhost/library'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class Books(db.Model):
+    bookID = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(255), nullable=False)
+    author = db.Column(db.Text)
+    year_published = db.Column(db.Integer)
+    Type = db.Column(db.String(10))
+    
+class Customers(db.Model):
+    custID = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(255), nullable=False)
+    city = db.Column(db.String(255))
+    age = db.Column(db.Integer)
+    password = db.Column(db.String(500))
+    
+class Loans(db.Model):
+    loanID = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    custID = db.Column(db.Integer, nullable=False)
+    bookID = db.Column(db.Integer, nullable=False)
+    LoanDate = db.Column(db.Date)
+    ReturnDate = db.Column(db.Date)
+
+
+# test route
+@app.route("/")
+def test():
+    return "test is on"
+
+# display all books
+@app.route("/show_books", methods=['GET'])
+def show_books():
+    if request.method == 'GET':
+        books = Books.query.all()
+        book_list = []
+
+        for book in books:
+            book_data = {
+                'id': book.bookID,
+                'name': book.name,
+                'author': book.author,
+                'year_published': book.year_published,
+                'Type': book.Type
+            }
+            book_list.append(book_data)
+
+        return jsonify({'books': book_list})
+
+# add new book to table books
+@app.route("/add_book", methods=['POST'])
+def add_book():
+    if request.method == 'POST':
+        data = request.get_json()
+
+        new_book = Books(
+            bookID=data['bookID'],
+            name=data['name'],
+            author=data['author'],
+            year_published=data['year_published'],
+            Type=data['Type']
+        )
+
+        db.session.add(new_book)
+        db.session.commit()
+
+        return jsonify({'message': 'Book added successfully'})
+    
+    
+    
+
+# show all customers 
+@app.route("/show_customers", methods=['GET'])
+def show_customers():
+    if request.method == 'GET':
+        customers = Customers.query.all()
+        customer_list = []
+        for customer in customers:
+            customer_data = {
+                'id': customer.custID,
+                'name': customer.name,
+                'city': customer.city,
+                'age': customer.age,
+                'password': customer.password
+            }
+            customer_list.append(customer_data)
+
+        return jsonify({'customers': customer_list})
+
+# add new customer to table customers
+@app.route("/add_customer", methods=['POST'])
+def add_customer():
+    if request.method == 'POST':
+        data = request.get_json()
+        new_customer = Customers(
+            custID = data['custID'],
+            name=data['name'],
+            city=data['city'],
+            age=data['age'],
+            password=data['password']
+        )
+        db.session.add(new_customer)
+        db.session.commit()
+        return jsonify({'message': 'Customer added successfully'})
+    
+@app.route("/show_loans", methods=['GET'])
+def show_loans():
+    if request.method == 'GET':
+        loans = Loans.query.all()
+        loan_list = []
+        for loan in loans:
+            loan_data = {
+                'id': loan.loanID,
+                'custID': loan.custID,
+                'bookID': loan.bookID,
+                'LoanDate': loan.LoanDate,
+                'ReturnDate': loan.ReturnDate
+            }
+            loan_list.append(loan_data)
+        return jsonify({'loans': loan_list})
+    
+@app.route("/loan_book", methods=['POST'])
+def loan_book():
+    if request.method == 'POST':
+        data = request.get_json()
+        new_loan = Loans(
+            loanID = data['loanID'],
+            custID=data['custID'],
+            bookID=data['bookID'],
+            LoanDate=data['LoanDate'],
+            ReturnDate=data['ReturnDate']
+        )
+        db.session.add(new_loan)
+        db.session.commit()
+        return jsonify({'message': 'Book loaned successfully'})
+
+@app.route("/return_book", methods=['POST'])
+def return_book():
+    if request.method == 'POST':
+        data = request.get_json()
+        loan = Loans.query.filter_by(loanID=data['loanID']).first()
+        loan.ReturnDate = data['ReturnDate']
+        db.session.commit()
+        return jsonify({'message': 'Book returned successfully'})
+        
+
+# entry point of the app 
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()  # Create the database tables before running the app
+    app.run(debug=True, port=5000)
